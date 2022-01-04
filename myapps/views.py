89ef7809fe2662448a9b911531565d6677c71cc3
Martin <bekzodsanakulov89@gmail.com>
2022-01-04 21:22:39 +0900

    feat: usage of migrated app's model as sources of db

--- myapps/views.py
+++ myapps/views.py
@@ -1,5 +1,6 @@
 from random import randint
 from django.http import HttpResponse
+from articles.models import Articles
 
 
 def home_view(request):
@@ -8,8 +9,9 @@ def home_view(request):
     :param request:
     :return:
     """
+    obj = Articles.objects.get(id=2)
     name = 'Martin'
     numb = randint(0, 100000)
-    HTML_ELEMENT = f'<h1>Hello mr. {name} for {numb} times</h1>'
+    HTML_ELEMENT = f'<h1>Hello mr. {name} for {numb} times on title: {obj.title}</h1>'
     return HttpResponse(HTML_ELEMENT)
 
